66688274b68e74972a62a67bd4ed4a05382a0674
JUDITH-10-NEC <sr.judith.v2003@gmail.com>
2026-01-19 17:01:53 +0530

    Initial calculator project

---
+++ script.js
@@ -0,0 +1,30 @@
+let display = document.getElementById("display");
+let buttons = document.querySelectorAll("button");
+
+let currentInput = "";
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+
+        let value = button.innerText;
+
+        if (value === "C") {
+            currentInput = "";
+            display.value = "";
+        }
+        else if (value === "=") {
+            try {
+                display.value = eval(currentInput);
+                currentInput = display.value;
+            } catch {
+                display.value = "Error";
+                currentInput = "";
+            }
+        }
+        else {
+            currentInput += value;
+            display.value = currentInput;
+        }
+
+    });
+});
